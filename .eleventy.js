ed6251157cda94afc0211342c50afcb1e0b2f155
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-06-20 12:07:19 -0700

    Added individual blog post layout, plus default front matter data for posts & a couple simple filter helpers.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
+// Utils
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
 module.exports = (config) => {
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
   config.addPassthroughCopy('./src/images/');
   // Returns work items, sorted by display order
   config.addCollection('work', (collection) => {
